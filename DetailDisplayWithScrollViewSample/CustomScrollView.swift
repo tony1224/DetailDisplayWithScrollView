6c685307a6ab465f03ae692d47383f9fb69be67f
Morita Jun <morita@onespal.co.jp>
2018-01-27 19:41:48 +0900

    first commit

---
+++ DetailDisplayWithScrollViewSample/CustomScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomScrollView.swift
+//  DetailDisplayWithScrollViewSample
+//
+//  Created by Morita Jun on 2018/01/27.
+//  Copyright © 2018年 test. All rights reserved.
+//
+
+import UIKit
+
+class CustomScrollView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
